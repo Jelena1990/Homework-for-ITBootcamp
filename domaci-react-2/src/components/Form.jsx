dfa4f5e78c1327c1bafc0300c1e372852c09e1b9
Jelena1990 <jvasilesku@gmail.com>
2023-02-20 18:08:41 +0100

    domaci2

--- domaci-react-2/src/components/Form.jsx
+++ domaci-react-2/src/components/Form.jsx
@@ -2,42 +2,61 @@ import React, { Component } from "react";
 
 class Form extends Component {
   state = {
-    name: "",
+    name: "hghh",
     lastname: "",
-    email: "",
+    mail: "",
     password: "",
   };
+  handleChange = (event) => {
+    //ovo name iz getAttribute("name") se odnosi na to kakvo sam ime dala inputu a ne na name iz state
+    let nameAttribute = event.target.getAttribute("name"); //od inputa koje sam postavila dole u render i dali mu name da li je ime name,lastname,password ili mail
+    if (nameAttribute === "ime") {
+      //jr nezimam name vec njegovo svojstvo, njegovu vrednst ja sam mogla i da
+      this.setState({
+        name: event.target.value,
+      });
+    } else if (nameAttribute === "prezime") {
+      this.setState({
+        lastname: event.target.value,
+      });
+    } else if (nameAttribute === "mail") {
+      this.setState({
+        mail: event.target.value,
+      });
+    } else if (nameAttribute === "password") {
+      this.setState({
+        password: event.target.value,
+      });
+    }
+  };
 
-  handleChange1 = (event) => {
-    console.log(event.target.value);
-    this.setState({ name: event.target.value });
-  };
-  handleChange2 = (event) => {
-    this.setState({ lastname: event.target.value });
-  };
-  handleChange3 = (event) => {
-    this.setState({ email: event.target.value });
-  };
-  handleChange4 = (event) => {
-    this.setState({ password: event.target.value });
-  };
+  componentDidUpdate() {
+    console.log(this.state); //metoda ima svoje unutrasnje metode funkcionise kao eventlistener za state i kad se na nesto klikne mogu da ga consologujem ili sta hocu da radim sa tim
+  }
 
   render() {
     return (
-      <form>
-        <h1>Hello {this.state.name} </h1>
-        <h3>Your lastname is {this.state.lastname} </h3>
-        <h3>Your email is {this.state.email} </h3>
-        <h3>Your password is {this.state.password} </h3>
-
-        <p>Enter your name</p>
-        <input type="text" onChange={this.handleChange1} />
-        <p>Enter your lastname</p>
-        <input type="text" onChange={this.handleChange2} />
-        <p>Enter your email</p>
-        <input type="email" onChange={this.handleChange3} />
-        <p>Enter your password</p>
-        <input type="password" onChange={this.handleChange4} />
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label name="ime">Name: </label>
+          <input type="text" name="ime" onChange={this.handleChange} />
+        </div>
+        <div>
+          <label name="prezime">Last name: </label>
+          <input type="text" name="prezime" onChange={this.handleChange} />
+        </div>
+        <div>
+          <label name="mail">e-mail: </label>
+          <input type="email" name="mail" onChange={this.handleChange} />
+        </div>
+        <div>
+          <label name="password">Password: </label>
+          <input type="password" name="password" onChange={this.handleChange} />
+        </div>
+        <h2> {this.state.name}</h2>
+        <h2> {this.state.lastname}</h2>
+        <h2> {this.state.mail}</h2>
+        <h2> {this.state.password}</h2>
       </form>
     );
   }
